9d820a11b1fca8b7432134033c5f06bd4c46141b
Pete Warden <pete@petewarden.com>
2011-03-18 16:22:44 -0700

    Added simple command-line interface

---
+++ boilerpipe-core/src/boilerpipe.java
@@ -0,0 +1,19 @@
+import java.net.URL;
+import java.util.Scanner;
+
+import de.l3s.boilerpipe.extractors.ArticleExtractor;
+
+class BoilerpipeCLI {
+  public static void main(String[] args) throws Exception {
+    
+    String inputText = "";
+    Scanner scanner = new Scanner(System.in);
+    while (scanner.hasNextLine()) {
+      inputText += scanner.nextLine()+"\n";
+    }
+
+    String output = ArticleExtractor.INSTANCE.getText(mockHtml);
+  
+    System.out.println(output);
+  }
+}
